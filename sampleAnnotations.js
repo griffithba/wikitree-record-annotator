ec3eab7553ae48acaa5a663fc5add99409f9d670
Ben Griffith <griffithba@gmail.com>
2026-05-04 22:23:43 -0400

    Add sample annotations

---
+++ sampleAnnotations.js
@@ -0,0 +1,136 @@
+// sampleAnnotations.js
+const sampleAnnotations = [
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 138.75,
+        "w": 241.52153110047857,
+        "x": 2652.9904306220096,
+        "y": 1599.3055555555557
+      },
+      {
+        "h": 123.68843283582078,
+        "w": 380.125,
+        "x": 2131.3650793650795,
+        "y": 1793.938432835821
+      }
+    ],
+    "death": null,
+    "id": "fa16268f-9043-4c49-8b59-28858baec674",
+    "name": null,
+    "note": "Father's cousin-in-law",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Andersson-8563"
+  },
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 148.7804878048782,
+        "w": 617.4298642533939,
+        "x": 2154.497737556561,
+        "y": 1931.0365853658536
+      }
+    ],
+    "death": null,
+    "id": "5c2f2896-4925-46b1-b9ab-1bc01c9db0a3",
+    "name": null,
+    "note": "Father's cousin",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Larsdotter-2942"
+  },
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 193.41463414634143,
+        "w": 490.9683257918553,
+        "x": 1615.1764705882351,
+        "y": 1692.9878048780488
+      }
+    ],
+    "death": null,
+    "id": "3f726f7d-a5d4-4d4d-a852-449dd682ab95",
+    "name": null,
+    "note": "",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Olsson-2483"
+  },
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 130.18292682926858,
+        "w": 531.8823529411764,
+        "x": 1626.3348416289593,
+        "y": 1990.5487804878048
+      }
+    ],
+    "death": null,
+    "id": "663a4e27-003e-4e1c-82c4-d55f23b4aab6",
+    "name": null,
+    "note": "",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Persdotter-3351"
+  },
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 137.62195121951208,
+        "w": 639.7466063348415,
+        "x": 2124.7420814479638,
+        "y": 2496.4024390243903
+      }
+    ],
+    "death": null,
+    "id": "f1acadb2-429a-45f0-a9cf-005ff97f2231",
+    "name": null,
+    "note": "Mother's sister",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Persdotter-3356"
+  },
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 297.82317073170725,
+        "w": 209.67857142857144,
+        "x": 773.2857142857142,
+        "y": 1635.6646341463415
+      }
+    ],
+    "death": null,
+    "id": "7c212e77-e3ab-4e1a-af04-6eff26c355eb",
+    "name": null,
+    "note": "",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Eriksson-3876"
+  },
+  {
+    "birth": null,
+    "boxes": [
+      {
+        "h": 143.39634146341496,
+        "w": 614.3214285714284,
+        "x": 2134.357142857143,
+        "y": 2234.987804878049
+      }
+    ],
+    "death": null,
+    "id": "439885ca-0cb1-4837-8c4b-0a6b27735c64",
+    "name": null,
+    "note": "Father's 1c1r",
+    "page": "C0012293_00425",
+    "status": "unknown",
+    "wtId": "Persson-10374"
+  }
+]
+    
