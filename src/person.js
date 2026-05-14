11a365f67b48f596f4d941eefc6401099d434be7
griffithba <49763576+griffithba@users.noreply.github.com>
2026-05-14 09:01:52 -0400

    Pull out persistent people caching

--- src/person.js
+++ src/person.js
@@ -1,67 +1,35 @@
 
 const people = new Map();
 
-// Cache expiration time (in milliseconds)
-const days = 14;
-const PERSON_CACHE_MAX_AGE_MS = days * 24 * 60 * 60 * 1000;
-
-const storageAPI = window.storage;
-
-
-// get person data
+// load person data into cache
 async function prefetchPerson(wtId) {
-  // fetch them from storage
-  person = await storageAPI.getPerson(wtId);
-  // if they were there then save them in cache
-  if (person) {
-    people.set(wtId, person);
+  // already cached
+  if (people.has(wtId)) {
+    return;
   }
-  
-  const stale =
-    !person ||
-    person.status !== "verified" ||
-    Date.now() - person.cachedAt > PERSON_CACHE_MAX_AGE_MS;
 
-  if (stale) {
-    const updatedPerson = await fetchPersonRecord(wtId);
-    people.set(wtId, updatedPerson);
-    console.log("Person cache miss or stale - fetched from API:", wtId);
-  } else console.log("Person cache hit (verified & fresh):", wtId);
-}
-
-// Request that the person data be fetched from the WikiTree API
-async function fetchPersonRecord(wtId) {
-
-  return new Promise((resolve) => {
-
+  const person = await new Promise((resolve) => {
     chrome.runtime.sendMessage(
       {
         type: "ENRICH_PERSON",
         wtId
       },
 
-      async (response) => {
-
+      (response) => {
         if (!response || response.error) {
-
           resolve(null);
-
           return;
         }
-
-        response.cachedAt = Date.now();
-
-        await storageAPI.savePerson(
-          wtId,
-          response
-        );
-
         resolve(response);
       }
     );
   });
+  if (person) {
+    people.set(wtId, person);
+  }
 }
 
+// pull person data from the cache
 function getCachedPerson(wtId) {
   return people.get(wtId);
 }
